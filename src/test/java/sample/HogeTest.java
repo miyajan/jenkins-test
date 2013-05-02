120cfb204e70d9a73f61954fa7b83f74423c8bf4
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2013-05-02 11:33:18 +0900

    fix not to fail.

--- src/test/java/sample/HogeTest.java
+++ src/test/java/sample/HogeTest.java
@@ -9,10 +9,8 @@ import org.junit.Test;
 
 import com.google.common.io.Files;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
-
-import static org.junit.Assert.*;
 
 public class HogeTest {
 
@@ -33,8 +31,6 @@ public class HogeTest {
         } catch (IOException e) {
             throw new AssertionError(e);
         }
-
-        fail();
     }
 
 }
