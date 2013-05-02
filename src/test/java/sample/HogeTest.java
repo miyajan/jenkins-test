3175289eafb227820f21aff921ec94e3a3ac04d4
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2013-05-02 11:05:53 +0900

    output file.

--- src/test/java/sample/HogeTest.java
+++ src/test/java/sample/HogeTest.java
@@ -1,6 +1,12 @@
 package sample;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.Charsets;
 import org.junit.Test;
+
+import com.google.common.io.Files;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -11,6 +17,13 @@ public class HogeTest {
     public void testFoo() {
         Hoge hoge = new Hoge();
         assertThat(hoge.foo(), is("bar"));
+
+        File file = new File("target/surefire-reports", "%HOGE%.txt");
+        try {
+            Files.write("hogehoge", file, Charsets.UTF_8);
+        } catch (IOException e) {
+            // do nothing.
+        }
     }
 
 }
